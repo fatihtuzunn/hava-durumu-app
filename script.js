9801b85b3069e95f318d29362b5a1b0c4ebd3d79
fatihtuzunn <fatihtuzun11@gmail.com>
2022-02-08 02:44:19 +0300

    update

---
+++ script.js
@@ -0,0 +1,42 @@
+const url ='http://api.openweathermap.org/data/2.5/';
+const key = 'c430638e277b0cbc6dcf678db55bc990';
+
+
+// 'enter' a basınca değer çekme fonskiyonu çalışsın.
+const setQuery = (e) =>{
+    if (e.keyCode == '13') {
+        getResult(searchBar.value)
+    }
+}
+
+
+
+const getResult = (cityName) => {
+    
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+    console.log(query);
+    
+    fetch(query)
+    .then(weather => {
+        return weather.json()
+    })
+    .then(displayResult) 
+}
+
+const displayResult = (result) => {
+    let city = document.querySelector(`.city`)
+    city.innerText = `${result.name}, ${result.sys.country}`
+
+    let temp = document.querySelector(`.temp`)
+    temp.innerText = `${result.main.temp} `
+
+    let desc = document.querySelector(`.desc`)
+    desc.innerText = `${result.weather[0].description}`
+
+}
+
+
+
+// index.html deki inputumuzu çekiyoruz.
+const searchBar = document.getElementById('search');
+searchBar.addEventListener('keypress', setQuery);
